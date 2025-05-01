cb032992e2134d7e4630aa735f6e4a8768be552a
Spookiss <jbeucher@et.esiea.fr>
2025-05-01 19:05:29 +0200

    Deployment

--- portfolio-site-2/src/components/Card.test.tsx
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const Card = require('../Card');
-
-test('hello world!', () => {
-	render(<Card />);
-	const linkElement = screen.getByText(/hello world/i);
-	expect(linkElement).toBeInTheDocument();
-});
